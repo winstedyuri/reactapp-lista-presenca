56df4f7ce390d76ff09879fdb9ddb718b1526465
Yuri Silva <yuricosta149@gmail.com>
2022-08-16 23:35:53 -0300

    first commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,60 @@
+import React, { useState, useEffect } from 'react'
 import './styles.css'
 
 import { Card } from '../../components/Card'
 
 export function Home() {
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+
+    setStudents(prevState => [...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <div className='container' >
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome..." />
-      <button type="button">Adicionar</button>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>Yuri Selva</strong>
+          <img src="https://github.com/winstedyuri.png" alt="Foto de Perfil" />
+        </div>
+      </header>
 
-      <Card name="Yuri" time="10:55:25" />
-      <Card name="Anderson" time="11:00:01" />
-      <Card name="Ana" time="12:13:44" />
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={e => setStudentName(e.target.value)}
+      />
+
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+
+      {
+        students.map(student => (
+          <Card
+            key={student.time}
+            name={student.name}
+            time={student.time} />
+        ))
+
+
+      }
+
     </div>
   )
 }
